ac5cbebe8f39c9ca6d24113f88dd211707eff44b
JohnGit1Hub1 <alexanderk7610@nycstudents.net>
2026-05-19 11:36:14 -0400

    Update Bottle.java

--- question-2/Bottle.java
+++ question-2/Bottle.java
@@ -1,3 +1,19 @@
 public class Bottle {
+    private double volume;
+    private double amt;
+
+    public Bottle(double v) {
+        volume = v;
+        amt = v;
+    }
+
+    public double updateAmount(double minusAmt) {
+        double temp = volume * 0.25;
+        amt -= minusAmt;
+        if (amt < temp) {
+            amt = volume;
+        }
+        return amt;
+    }
 
 }
