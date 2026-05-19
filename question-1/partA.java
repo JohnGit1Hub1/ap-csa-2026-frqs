dc16087881eef9403330003180b199b1f8fb2d33
JohnGit1Hub1 <alexanderk7610@nycstudents.net>
2026-05-19 11:51:40 -0400

    Update partA.java

--- question-1/partA.java
+++ question-1/partA.java
@@ -1,1 +1,9 @@
-
+public Account(String requestedName) {
+    String name = requestedName;
+    int i = 0;
+    while (!isAvailable(name)) {
+        name = name + i;
+        i++;
+    }
+    username = name;
+}
