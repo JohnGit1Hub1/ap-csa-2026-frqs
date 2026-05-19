972e318cd6728debf322654ce0656fd99ea3da6c
JohnGit1Hub1 <alexanderk7610@nycstudents.net>
2026-05-19 15:52:31 -0400

    Update partB.java

--- question-1/partB.java
+++ question-1/partB.java
@@ -1,1 +1,10 @@
-
+public String getShortenedName() {
+    String name = username;
+    while (username.indexOf("-") != -1) {
+        int start = username.indexOf("-") - 1;
+        int end = start + 2;
+        
+        name = name.substring(0, start) + name.substring(start, end);
+    }   
+    return name;
+}
