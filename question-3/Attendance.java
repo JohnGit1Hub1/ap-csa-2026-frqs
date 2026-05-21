a6020e8d10ee0c9e1b5b7837aff10a0fa36756c9
JohnGit1Hub1 <alexanderk7610@nycstudents.net>
2026-05-21 11:46:01 -0400

    Update Attendance.java

--- question-3/Attendance.java
+++ question-3/Attendance.java
@@ -1,1 +1,13 @@
-
+public int moreHistoryThanMathAbsences() {
+    int count = 0;
+    for (CourseRecord sH : historyList) {
+        for (CourseRecord sM : mathList) {
+            if (sM.getStudentID().equals(sH.getStudentID())) {
+                if (sH.getAbsences() > sM.getAbsences()) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
